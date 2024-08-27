3e653b5136d938a4cadf4fd5384eb3f142b8045d
iamsushantjadhav@gmail.com <sushant.j@codewalla.com>
2024-08-27 20:18:29 +0530

    fix test cases

--- src/main/java/com/apptware/interview/spring/beans/BeanFactory.java
+++ src/main/java/com/apptware/interview/spring/beans/BeanFactory.java
@@ -7,9 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class BeanFactory {
 
-  @Autowired private ApplicationContext context;
 
   public OnDemand getOnDemandBean(SomeEnum someEnum, String someString) {
-    return context.getBean(BaseOnDemand.class, someString);
+    if (someEnum == SomeEnum.SOME_ENUM_A) {
+      return new OnDemandA(someString);
+    } else if (someEnum == SomeEnum.SOME_ENUM_B) {
+      return new OnDemandB(someString);
+    } else {
+      throw new IllegalArgumentException("Unknown enum type: " + someEnum);
+    }
   }
 }
